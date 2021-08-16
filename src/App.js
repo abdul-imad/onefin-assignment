a86ab00c6b30031b9a3652dae2c3a4ce24fd39b4
Abdul Imad <abdimad786@gmail.com>
2021-08-16 16:07:26 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/login" component={Login} />
+				<Route path="/" component={Dashboard} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
